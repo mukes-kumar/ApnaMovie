20505c20ebe4fb37d24954960ef968b95934ddfa
Mukesh Prajapati <145520335+mukes-kumar@users.noreply.github.com>
2024-11-03 12:11:57 +0530

    All Changes done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <footer className='text-center bg-neutral-600 bg-opacity-35 text-neutral-400 py-2'>
-        <div className='flex items-center justify-center gap-4'>
+    <footer className='text-center bg-gradient-to-l from-red-800 to-slate-800 bg-opacity-35 text-slate-50 py-2'>
+        <div className='flex items-center justify-center gap-4 '>
           <Link to="/" >About</Link>
           <Link to="/">Contact</Link>
         </div>
-        <a className='text-sm' href='https://www.linkedin.com/in/mukesh-prajapati-751340260'>Created by Mukesh Prajapati</a>
+        <a className='text-sm text-blue-200' href='https://www.linkedin.com/in/mukesh-prajapati-751340260'>Created by Mukesh Prajapati</a>
     </footer>
   )
 }
